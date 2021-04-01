12931de52664fe4307d38f2d2cd2e32a5e7bbcca
Avigyan09 <81364535+Avigyan09@users.noreply.github.com>
2021-04-01 12:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
 
-app = Flask(__avigyan__)
+app = Flask(__name__)
 model = pickle.load(open('rfc.pkl','rb'))
 
 @app.route('/')
